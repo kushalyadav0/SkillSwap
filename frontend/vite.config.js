c408fe4887a6309348cd66b935e441326ea51aad
Laxman <theprogrammer452023@gmail.com>
2025-07-12 16:24:10 +0530

    update vite config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  build: {
+    outDir: 'dist', // Vite default, but make sure Django looks for this
+    emptyOutDir: true,
+  },
+
       
 })
+
+
